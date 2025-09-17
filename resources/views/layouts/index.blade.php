bd8cb3880837fc40c5e388377d24f2d8d62e9d50
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-09-17 19:07:35 +0600

    Commit Wed 09/17/2025 19:07:35.27 7077

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -40,14 +40,6 @@
   <link href="{{ asset('vendor/bloggy/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/bloggy/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-
-
   <!-- Main CSS File -->
   <link href="{{ asset('vendor/bloggy/assets/css/main.css') }}" rel="stylesheet">
   @yield('third_party_stylesheets')
